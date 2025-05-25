7ce6059666fb539e06ec0f20c81d711ba98e5ceb
rsolanov <solanov.roman@yandex.ru>
2025-05-25 21:36:12 +0300

    StringConcatenator: упрощение кода

--- hw-02/src/main/scala/StringConcatenator.scala
+++ hw-02/src/main/scala/StringConcatenator.scala
@@ -1,10 +1,7 @@
+package example
 object StringConcatenator {
-  def concatStrings(strings: List[String]): String = {
-    strings.mkString(" ")
-  }
-
   def main(args: Array[String]): Unit = {
     val words = List("Scala", "является", "мощным", "языком", "программирования")
-    println("Объединенная строка: " + concatStrings(words))
+    println("Объединенная строка: " + words.mkString(" "))
   }
 }
