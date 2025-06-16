9acc11cdec67883c1c2bc57055a46d204deb4262
rsolanov <solanov.roman@yandex.ru>
2025-06-16 03:45:48 +0300

    PEP8

--- hw-11/python/main.py
+++ hw-11/python/main.py
@@ -1,19 +1,21 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import col, size, concat_ws
 
+
 def get_spark():
     return SparkSession.builder \
-    .appName("SparkDataFrameAPI") \
-    .master("local[*]") \
-    .config("spark.sql.adaptive.enabled", "true") \
-    .config("spark.sql.adaptive.coalescePartitions.enabled", "true") \
-    .config("spark.sql.debug.maxToStringFields", "1000") \
-    .config("spark.sql.log.level", "ERROR") \
-    .getOrCreate()
+        .appName("SparkDataFrameAPI") \
+        .master("local[*]") \
+        .config("spark.sql.adaptive.enabled", "true") \
+        .config("spark.sql.adaptive.coalescePartitions.enabled", "true") \
+        .config("spark.sql.debug.maxToStringFields", "1000") \
+        .config("spark.sql.log.level", "ERROR") \
+        .getOrCreate()
 
-def getCountriesWithManyBorders(df: DataFrame, minBorders: int = 5) -> DataFrame:
+
+def get_countries_with_many_borders(df: DataFrame, min_borders: int = 5) -> DataFrame:
     return (
-        df.filter((col("borders").isNotNull()) & (size(col("borders")) >= minBorders))
+        df.filter((col("borders").isNotNull()) & (size(col("borders")) >= min_borders))
         .select(
             col("name.common").alias("Country"),
             size(col("borders")).alias("NumBorders"),
@@ -22,27 +24,29 @@ def getCountriesWithManyBorders(df: DataFrame, minBorders: int = 5) -> DataFrame
         .orderBy(col("NumBorders").desc())
     )
 
+
 def main():
     sp = get_spark()
     try:
-        countriesDF = sp.read.option("multiLine", value = True).json("countries.json")
+        countries_df = sp.read.option("multiLine", value=True).json("countries.json")
 
-        countriesDF.cache()
+        countries_df.cache()
 
         print("Схема данных:")
-        countriesDF.printSchema()
+        countries_df.printSchema()
 
         print("Пример данных:")
-        countriesDF.select("name.common", "borders").show(5, truncate = False)
+        countries_df.select("name.common", "borders").show(5, truncate=False)
 
-        bordersDF = getCountriesWithManyBorders(countriesDF)
+        borders_df = get_countries_with_many_borders(countries_df)
         print("Страны с 5+ границами:")
 
-        bordersDF.show(5, False)
+        borders_df.show(5, False)
     except Exception as e:
         print(str(e))
     finally:
         sp.stop()
+
 
 if __name__ == "__main__":
     main()
